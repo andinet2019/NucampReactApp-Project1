6a86259d6f02affecf7e3d472b9c6ae67469a103
Andinet <andinetmn@gmail.com>
2020-02-29 11:19:04 -0600

    workshop 4

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Main from "./components/MainComponent";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
+import Loading from "./components/LoadingComponent";
+import { ConfigureStore } from "./redux/configureStore";
+
+const { persistor, store } = ConfigureStore();
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <Main />
+      </PersistGate>
+    </Provider>
+  );
+}
+console.disableYellowBox = true;
---
+++ shared/baseUrl.js
@@ -0,0 +1,1 @@
+export const baseUrl = "http://10.178.110.42:3001/";
